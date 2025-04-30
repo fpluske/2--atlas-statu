881613000dac075d879f72f1bc4a2cbb740c01cf
fpluske <frantisek.pluske@seznam.cz>
2025-04-30 11:44:32 +0200

    V 1.2 - Poslední úpravy

--- atlas.js
+++ atlas.js
@@ -7,8 +7,8 @@ const modal = new bootstrap.Modal(document.getElementById("oneCountry"));
 function loadCountries(region) {
   countriesList.innerHTML = "";
   fetch(`https://restcountries.com/v3.1/region/${region}`)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       console.log(data);
       data.forEach((country) => {
         let blockCountry = `
@@ -28,37 +28,45 @@ function loadCountries(region) {
           `;
         countriesList.innerHTML += blockCountry;
       });
-      document.querySelectorAll('button[data-name]').forEach(button => {
-        button.addEventListener('click', () => {
-          const countryName = button.getAttribute('data-name');
+      document.querySelectorAll("button[data-name]").forEach((button) => {
+        button.addEventListener("click", () => {
+          const countryName = button.getAttribute("data-name");
           modal.show();
-          fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText-true`)
-          .then(res => res.json())
-          .then(data => {
-            const country = data[0];
-            let sousedi = "";
-            for (let i = 0; i < country.borders.length; i++) {
-              sousedi += (i > 0 ? ", " : "") + country.borders[i];
-            }
-            modalTitle.innerHTML = `<h4>${country.translations.ces.common}</h4>`;
-            modalBody.innerHTML = `
+          fetch(
+            `https://restcountries.com/v3.1/name/${countryName}?fullText-true`
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              const country = data[0];
+              let sousedi = "";
+              for (let i = 0; i < country.borders.length; i++) {
+                sousedi += (i > 0 ? ", " : "") + country.borders[i];
+              }
+              modalTitle.innerHTML = `<h4>${country.translations.ces.common}</h4>`;
+              modalBody.innerHTML = `
             <img src="${country.flags.png}" alt="Vlajka ${country.name.common}">
                   <p></p>
                   <p><b>Hlavní město: </b>${country.capital[0]}</p>
                   <p><b>Rozloha: </b>${country.area} km<sup>2</sup></p>
                   <p><b>Sousedí s: </b>${sousedi}</p>
                   <p><b>Populace: </b>${country.population} lidí</p>
-                  <p><b>Měna: </b>${Object.values(country.currencies)[0].name}</p>
-                  <p><b>Nachází se: </b><a href="${country.maps.googleMaps}" target="_blank">Google mapy</a> <a href="${country.maps.openStreetMaps}" target="_blank">Open Street Maps</a></p>
+                  <p><b>Měna: </b>${
+                    Object.values(country.currencies)[0].name
+                  }</p>
+                  <p><b>Nachází se: </b><a href="${
+                    country.maps.googleMaps
+                  }" target="_blank">Google mapy</a> <a href="${
+                country.maps.openStreetMaps
+              }" target="_blank">Open Street Maps</a></p>
                   <p><b>Časová zóna: </b>${country.timezones[0]}</p>`;
-          })
-          .catch(error => {
-            console.error(error);
-          })
-        })
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        });
       });
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
     });
 }
